f2d8e4c9292edd4f574ef189b8365794e31363d2
todd <todd@nyx.OLYMPUS>
2017-09-11 15:53:32 -0400

    Fixing formatting.

--- ggService/src/main/java/org/outsrcd/ggService/Launcher.java
+++ ggService/src/main/java/org/outsrcd/ggService/Launcher.java
@@ -7,31 +7,30 @@ import org.apache.camel.impl.SimpleRegistry;
 
 public class Launcher {
 
-	public static void main(String... args) throws Exception {		
-		
-		// if no arg for replay directory to scan, 
-		if (args == null || args.length < 1)
-		{
+	public static void main(String... args) throws Exception {
+
+		// if no arg for replay directory to scan,
+		if (args == null || args.length < 1) {
 			System.out.println("Must provide argument for directory to be scanned");
 			System.exit(1);
 		}
-		
+
 		// add the MultipartUploadBean to registry, so we can access it in routes
 		MultipartUploadBean uploadBean = new MultipartUploadBean();
-	    SimpleRegistry reg = new SimpleRegistry();
-	    reg.put("uploadBean", uploadBean);
-		
-	    // add properties file to classpath
-	    PropertiesComponent props = new PropertiesComponent();
-		props.setLocation("classpath:config.properties");
-				
-		// start the context, passing in the replay directory to scan
-	    CamelContext context = new DefaultCamelContext(reg);
-		context.addComponent("properties", props);
-	    context.addRoutes(new UploadRoute(args[0]));
-	    context.start();
+		SimpleRegistry reg = new SimpleRegistry();
+		reg.put("uploadBean", uploadBean);
 
-	    // wait
-	    Thread.currentThread().join();	    
+		// add properties file to classpath
+		PropertiesComponent props = new PropertiesComponent();
+		props.setLocation("classpath:config.properties");
+
+		// start the context, passing in the replay directory to scan
+		CamelContext context = new DefaultCamelContext(reg);
+		context.addComponent("properties", props);
+		context.addRoutes(new UploadRoute(args[0]));
+		context.start();
+
+		// wait
+		Thread.currentThread().join();
 	}
 }
