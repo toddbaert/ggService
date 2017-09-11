0270c1dfdef3c3d186d3164552ef79dd007de316
todd <todd@nyx.OLYMPUS>
2017-09-11 11:56:27 -0400

    adding final upload functionality.

--- ggService/src/main/java/org/outsrcd/ggService/Launcher.java
+++ ggService/src/main/java/org/outsrcd/ggService/Launcher.java
@@ -1,13 +1,24 @@
 package org.outsrcd.ggService;
 
-import org.apache.camel.main.Main;
+import org.apache.camel.CamelContext;
+import org.apache.camel.component.properties.PropertiesComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.SimpleRegistry;
 
 public class Launcher {
 
-	public static void main(String... args) throws Exception {
-		Main main = new Main();
-		main.addRouteBuilder(new CamelRoute());
-		main.run(args);
-
+	public static void main(String... args) throws Exception {		
+		
+		MultipartUploadBean uploadBean = new MultipartUploadBean();
+	    SimpleRegistry reg = new SimpleRegistry();
+	    reg.put("uploadBean", uploadBean);
+		
+	    PropertiesComponent props = new PropertiesComponent();
+		props.setLocation("classpath:config.properties");
+		
+	    CamelContext context = new DefaultCamelContext(reg);
+		context.addComponent("properties", props);
+	    context.addRoutes(new UploadRoute());
+	    context.start();
 	}
 }
---
+++ ggService/src/main/java/org/outsrcd/ggService/UploadRoute.java
@@ -0,0 +1,55 @@
+package org.outsrcd.ggService;
+
+import java.io.InputStream;
+import java.util.HashMap;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpMethods;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class UploadRoute extends RouteBuilder {
+
+	class SignatureRequestAggregationStrategy implements AggregationStrategy {
+		
+		@SuppressWarnings("unchecked")
+		@Override
+		public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+
+			HashMap<String, Object> responseMap = null;
+			try (InputStream inputStream = (InputStream) newExchange.getIn().getBody()) {
+				responseMap = new ObjectMapper().readValue(inputStream, HashMap.class);
+			} catch (Exception e) {
+				log.error("Error parsing signature response", e);
+			}
+			
+			responseMap.forEach((key, value) -> {
+				oldExchange.getIn().setHeader(key, value);
+			});
+			
+			return oldExchange;
+		}
+	}
+
+	@Override
+	public void configure() throws Exception {
+
+		// scan replay directory every 60 seconds for new replay
+		from("file:/home/todd/temp/?noop=true&recursive=true&delay=60000")
+			
+			// throttle to 1 upload per second max as per ggTracker request
+			.throttle(1)
+			.log("found ${file:onlyname}")
+			
+			// make request to sign the upload for s3 using enrich EIP
+			.setHeader(Exchange.HTTP_QUERY, simple("doc[title]=${file:onlyname}"))
+			.setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.GET)).log(">>> ${header.CamelHttpQuery}")
+			.enrich("http4://{{config.signature.host}}", new SignatureRequestAggregationStrategy())
+			
+			// perform multipart upload to s3
+			.doTry().to("bean:uploadBean?method=send").endDoTry()			
+			.doCatch(Exception.class).log(">>> message: ${exception.message}").log(">>> trace: ${exception.stacktrace}").end();
+	}
+}
