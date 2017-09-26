99605cc9853ddc2cf8fdb93267408b8a5ca0f09b
Todd Baert <toddbaert@gmail.com>
2017-09-26 12:26:22 -0400

    changing args to system properties

--- ggService/src/main/java/org/outsrcd/ggService/Launcher.java
+++ ggService/src/main/java/org/outsrcd/ggService/Launcher.java
@@ -7,35 +7,34 @@ import org.apache.camel.impl.SimpleRegistry;
 
 public class Launcher {
 
-	public static void main(String... args) throws Exception {
+  public static void main(String... args) throws Exception {
 
-		// if no arg for replay directory to scan,
-		if (args == null || args.length < 3) {
-			System.out.println("args must include: replay directory, user name, and passowrd");
-			System.exit(1);
-		} else {
-			System.setProperty("replayDir", args[0]);
-			System.setProperty("user", args[1]);
-			System.setProperty("password", args[2]);
-		}
-		
-		SimpleRegistry reg = new SimpleRegistry();
-		
-		// add the MultipartUploadBean to registry, so we can access it in routes
-		MultipartUploadBean multipartUploadBean = new MultipartUploadBean();
-		reg.put("multipartUploadBean", multipartUploadBean);
+    // if no arg for replay directory to scan,
+    if (System.getProperty("replayDir") == null 
+        || System.getProperty("user") == null
+        || System.getProperty("password") == null) {
+    
+      System.out.println("args must include: replay directory, user name, and password");
+      System.exit(1);
+    }
 
-		// get properties file so we can add it to the context
-		PropertiesComponent props = new PropertiesComponent();
-		props.setLocation("classpath:config.properties");
+    SimpleRegistry reg = new SimpleRegistry();
 
-		// start the context, passing in the replay directory to scan
-		CamelContext context = new DefaultCamelContext(reg);
-		context.addComponent("properties", props);
-		context.addRoutes(new UploadRoute());
-		context.start();
+    // add the MultipartUploadBean to registry, so we can access it in routes
+    MultipartUploadBean multipartUploadBean = new MultipartUploadBean();
+    reg.put("multipartUploadBean", multipartUploadBean);
 
-		// wait
-		Thread.currentThread().join();
-	}
+    // get properties file so we can add it to the context
+    PropertiesComponent props = new PropertiesComponent();
+    props.setLocation("classpath:config.properties");
+
+    // start the context, passing in the replay directory to scan
+    CamelContext context = new DefaultCamelContext(reg);
+    context.addComponent("properties", props);
+    context.addRoutes(new UploadRoute());
+    context.start();
+
+    // wait
+    Thread.currentThread().join();
+  }
 }
